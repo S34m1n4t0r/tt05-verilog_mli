bcbee96e2a4ffcd6f7c58ff37daaa3e9343cbbfa
S34m1n4t0r <81908108+S34m1n4t0r@users.noreply.github.com>
2023-10-05 07:11:10 +0000

    value statement

--- src/test.py
+++ src/test.py
@@ -22,13 +22,13 @@ async def test_polarity_low(dut):
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 100)
     
-    dut.ui_in = 1
+    dut.ui_in.value = 1
     await ClockCycles(dut.clk, 10)
     dut._log.info("Value of dut.uo_out: {}".format(int(dut.uo_out.value)))
     assert int(dut.uo_out.value) == 0
 
     await ClockCycles(dut.clk, 100)
-    dut.ui_in = 0
+    dut.ui_in.value = 0
     
     
     #dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
